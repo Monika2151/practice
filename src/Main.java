6ddf37e64aa8385942cdb71a7e41d07a56f90026
monika <monikatanwar2151@gmail.com>
2025-05-19 20:34:58 +0530

    update main file

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("hello world" );
+        System.out.printf("my name is monika" );
     }
 }
